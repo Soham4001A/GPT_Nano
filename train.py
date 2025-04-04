24917bc01c2cd1a3370806a7b1dc3777e19df4c7
Soham Sane <sohamsane@Sohams-MacBook-Pro.local>
2025-04-04 00:05:09 -0400

    didnt save learning rate

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ bias = False # do we use bias inside LayerNorm and Linear layers?
 use_lma = False
 lma_reduction_factor = 2
 # adamw optimizer
-learning_rate = 6e-4 # max learning rate (changed back from 2e-4 based on common baseline)
+learning_rate = 2e-4 # max learning rate (changed back from 2e-4 based on common baseline)
 max_iters = 600000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
