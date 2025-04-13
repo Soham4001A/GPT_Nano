4a8df75c7fb3352e7bd93aac98f37b6e6f89b82a
Soham Sane <sohamsane@Sohams-MacBook-Pro.local>
2025-04-13 10:42:31 -0400

    Lowering Learning Rate slightly

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ use_gated_reduction = True # Default to standard GPT
 # gating_d_new will be calculated after config loading based on n_embd & d_reduction_factor
 # -----------------------------------------
 # adamw optimizer
-learning_rate = 3e-3 # max learning rate
+learning_rate = 9e-4 # max learning rate
 max_iters = 600000 # total number of training iterations
 weight_decay = 0.07
 beta1 = 0.9
